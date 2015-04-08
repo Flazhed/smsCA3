264d75d4bb031a536fda200ab804cd110f11fc98
Flazhed <soren1337@gmail.com>
2015-04-08 12:23:57 +0200

    Quotefacade update 1

--- model/publicQuoteFacade.js
+++ model/publicQuoteFacade.js
@@ -9,11 +9,11 @@ function _getAllTopics(callback){
     Quote.find().distinct('topic', function(err, result){
 
         if(err){
-            console.log(err)
+            callback(err)
         }
         else {
-            console.log(JSON.stringify(result));
-            callback(JSON.stringify(result))
+            //console.log(JSON.stringify(result));
+            callback(null, JSON.stringify(result))
         }
     })
 
@@ -25,21 +25,45 @@ function _getQuoteByTopic(topicInput, callback){
     //Return json list with all quotes matching a topic
     Quote.find({topic: topicInput}, function(err, result){
         if(err){
-            console.log(err)
+            callback(err)
         }
         else {
-            console.log(JSON.stringify(result));
-            callback(JSON.stringify(result))
+            //console.log(JSON.stringify(result));
+            callback(null, JSON.stringify(result))
         }
     })
 
 }
 
-function _getRandomQuoteByTopic(topic, callback){
+function _getRandomQuoteByTopic(topicInput, callback){
 
+    Quote.count({topic: topicInput}, function(err, c){
+        if(err){
+            callback(err)
+        }
+        else {
+            var random = Math.floor(Math.random() * c);
+            console.log(c + " random: " + random);
+            Quote.findOne({topic: topicInput}).skip(random).exec(
+                function (err, result) {
+                    if(err){return console.log(err)}
+                    console.log(JSON.stringify(result))
+                    //callback(null, JSON.stringify(result))
+                });
+
+
+        }
+    })
     //return a single json-quote randomly from the given topic
 
 }
 
 //_getAllTopics()
 //_getQuoteByTopic("Fiskecitater");
+_getRandomQuoteByTopic("Sports");
+
+module.exports = {
+    getAllTopics : _getAllTopics,
+    getQuoteByTopic : _getQuoteByTopic,
+    getRandomQuoteByTopic : _getRandomQuoteByTopic
+}
